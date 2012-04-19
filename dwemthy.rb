108861ad6799714da5e2426a0c7dd52c3804bf5a
Luis Borjas <luisfborjas@gmail.com>
2012-04-18 23:46:18 -0600

    Paso 3: métodos de instancia del juego

--- dwemthy.rb
+++ dwemthy.rb
@@ -8,10 +8,13 @@ class Creature
     @weapon = weapon
   end
 
+
   attr_reader :life, :strength, :charisma, :weapon, :name
   #attr_writer :name
   attr_accessor :name
   
+  alias :life_points :life
+
   def self.create_random
     random_name = ('a'..'z').to_a.sample(10).join.capitalize
     Creature.new rand(1..100), rand(1..10), rand(1..100), rand(1..100), random_name
@@ -19,6 +22,49 @@ class Creature
 
 
   def to_s
-    "#{self.class}: life = #{life}, weapon = #{weapon}"
+    "#{@name}: life = #{life}, weapon = #{weapon}"
   end
+
+  def dead?
+    @life <= 0
+  end
+
+  def alive?
+    @life > 0
+  end
+
+  def hit(damage)
+    p_up = rand( charisma )
+     if p_up % 9 == 7
+       @life += p_up / 4
+       puts "[#{ @name } magick powers up #{ p_up }!]" 
+     end 
+     @life -= damage
+     puts "[#{ @name } has died.]" if dead?
+  end
+
+  alias :get_hit :hit
+
+  def fight( enemy, weapon = nil )
+   weapon ||= @weapon
+   if dead?
+     puts "[#{ @name } is too dead to fight!]" 
+     return
+   end
+
+   # Attack the opponent
+   your_hit = rand( strength + weapon )
+   puts "[#{@name} hits #{enemy.name} with #{ your_hit } points of damage!]" 
+   enemy.hit( your_hit )
+
+   # Retaliation
+   p enemy
+   if enemy.alive?
+     enemy_hit = rand( enemy.strength + enemy.weapon )
+     puts "[ #{enemy.name} hits #{@name}  back with #{ enemy_hit } points of damage!]" 
+     self.hit( enemy_hit )
+   end
+  end
+
+  alias :attack :fight
 end
